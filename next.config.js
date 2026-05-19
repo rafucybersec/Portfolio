8ef96fdc64a320579591ddf8637a408080003dad
Muhammad Rafay Ali <muhammad.rafayali@outlook.com>
2026-05-20 00:40:11 +0500

    Remove trailingSlash configuration from nextConfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: false,
   output: 'standalone',
   reactCompiler: false,
   experimental: {
