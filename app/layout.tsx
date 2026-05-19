900727a1391ff5e5dbeb5ab68ac89d4de4247935
Muhammad Rafay Ali <muhammad.rafayali@outlook.com>
2026-05-19 09:12:54 +0500

    Add verification codes for Google and Yandex
    Added Google and Yandex verification codes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,9 +97,8 @@ export const metadata: Metadata = {
     },
   },
   verification: {
-    // Add your verification codes here if needed
-    // google: 'your-google-verification-code',
-    // yandex: 'your-yandex-verification-code',
+    google: 'ElRIclZ7K1Uy3Nuuwc9z8h5qeUByzlZ6mQJjW4dM-B8',
+    yandex: '1860ae3b4662ff06',
   },
 }
 
